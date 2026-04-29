f25dff00e9a8c9551e3051e4c5e619a307399edd
G Sai Charan Teja <gavidisaicharanteja@gmail.com>
2026-04-29 09:04:31 +0530

    Update EmailService.java

--- src/main/java/com/example/CivicConnect/EmailService.java
+++ src/main/java/com/example/CivicConnect/EmailService.java
@@ -1,11 +1,31 @@
 package com.example.CivicConnect;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
     public void sendOtp(String toEmail, String otp) {
-        System.out.println("OTP for " + toEmail + " is: " + otp);
+
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(toEmail);
+            message.setSubject("OTP Verification");
+            message.setText("Your OTP is: " + otp);
+
+            mailSender.send(message);
+
+            System.out.println("MAIL SENT ✅");
+
+        } catch (Exception e) {
+            System.out.println("MAIL FAILED ❌");
+            e.printStackTrace();
+        }
     }
 }
