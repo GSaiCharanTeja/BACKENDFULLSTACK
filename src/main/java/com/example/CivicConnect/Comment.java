4cc845b130585225fce9bda95c96269fdaa3c365
G Sai Charan Teja <gavidisaicharanteja@gmail.com>
2026-04-29 06:20:24 +0530

    Update Comment.java

--- src/main/java/com/example/CivicConnect/Comment.java
+++ src/main/java/com/example/CivicConnect/Comment.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.*;
 @Entity
 public class Comment {
 
