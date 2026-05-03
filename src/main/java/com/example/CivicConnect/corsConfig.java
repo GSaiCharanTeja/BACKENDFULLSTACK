b8d6c5fd7585a26bc9374e9f1848ede2d437c508
G Sai Charan Teja <gavidisaicharanteja@gmail.com>
2026-05-03 10:50:46 +0530

    Update corsConfig.java

--- src/main/java/com/example/CivicConnect/corsConfig.java
+++ src/main/java/com/example/CivicConnect/corsConfig.java
@@ -13,7 +13,10 @@ public class corsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                            "http://localhost:5173",
+                            "https://fullstack-frontend-djv7.vercel.app"
+                        )
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
